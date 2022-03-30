3ec19efbfe479e5c4df566d25ed490b768ca0253
unknown <john.tamad147@gmail.com>
2022-03-30 14:49:02 +0800

    set up sanctum

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,14 @@ Route::get('/products/search/{name}', [ProductController::class, 'search']);
 //         ]
 //         );
 // });
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    //Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
